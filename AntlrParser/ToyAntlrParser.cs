73ecbe0e02813b9c3c8f429ae120ca0b80c07e5f
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2021-12-16 20:59:15 -0600

    Update ToyAntlrParser.cs

--- AntlrParser/ToyAntlrParser.cs
+++ AntlrParser/ToyAntlrParser.cs
@@ -13,33 +13,12 @@ namespace AntlrParser
             var str = CharStreams.fromString(text);
 
             var lexer = new CoolLexer(str);
-
-            var newS = new StringBuilder();
-
-            while (true)
-            {
-                var roToken = lexer.NextToken();
-                var token = (CommonToken)roToken;
-
-                if (token.Channel == TokenConstants.DefaultChannel)
-                {
-                    if (token.Type == TokenConstants.EOF)
-                    {
-                        break;
-                    }
-
-                    newS.AppendLine(lexer.Vocabulary.GetSymbolicName(token.Type) + ": " + token.Text);
-                }
-            }
-
-            Console.Error.WriteLine(newS.ToString());
-
-            lexer.Reset();
-
             var tokens = new CommonTokenStream(lexer);
             var parser = new CoolParser(tokens);
+            
             var listenerLexer = new ErrorListener<int>();
             var listenerParser = new ErrorListener<IToken>();
+            
             lexer.AddErrorListener(listenerLexer);
             parser.AddErrorListener(listenerParser);
 
